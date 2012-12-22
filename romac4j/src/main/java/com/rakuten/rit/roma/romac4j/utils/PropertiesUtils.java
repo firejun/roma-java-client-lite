818759921aa9c054bc978fdb8a324efdfb5c0a5c
firejun <fire@jun-t.com>
2012-12-22 17:18:20 +0900

    Change method type.

--- romac4j/src/main/java/com/rakuten/rit/roma/romac4j/utils/PropertiesUtils.java
+++ romac4j/src/main/java/com/rakuten/rit/roma/romac4j/utils/PropertiesUtils.java
@@ -4,38 +4,33 @@ import java.io.FileInputStream;
 import java.util.Properties;
 
 public class PropertiesUtils {
-	private Properties props;
-	private static final String CONFIG_FILE = "romaclient.properties";
-	private static final String DEFAULT_ROMA_ADDRESS = "localhost";
-	private static final String DEFAULT_ROMA_PORT = "11211";
-	private static final String DEFAULT_NUM_OF_CONNECTION = "10";
-	private static final String DEFAULT_MAX_ACTIVE = "10";
-	private static final String DEFAULT_MAX_IDLE = "10";
-	private static final String DEFAULT_TIMEOUT = "1000";
-	private static final String DEFAULT_BUFFER_SIZE = "1024";
+    private static final String CONFIG_FILE = "romaclient.properties";
+    private static final String DEFAULT_ROMA_ADDRESS = "localhost_11111";
+    private static final String DEFAULT_MAX_ACTIVE = "10";
+    private static final String DEFAULT_MAX_IDLE = "10";
+    private static final String DEFAULT_TIMEOUT = "1000";
+    private static final String DEFAULT_EXP_TIMEOUT = "10000";
+    private static final String DEFAULT_BUFFER_SIZE = "1024";
+    private static final String DEFAULT_MAX_RETRY = "5";
 
-	public PropertiesUtils() {
-		props = new Properties();
-		props.setProperty("address", DEFAULT_ROMA_ADDRESS);
-		props.setProperty("port", DEFAULT_ROMA_PORT);
-		props.setProperty("numOfConnection", DEFAULT_NUM_OF_CONNECTION);
-		props.setProperty("maxActive", DEFAULT_MAX_ACTIVE);
-		props.setProperty("maxIdle", DEFAULT_MAX_IDLE);
-		props.setProperty("timeout", DEFAULT_TIMEOUT);
-		props.setProperty("bufferSize", DEFAULT_BUFFER_SIZE);
-		try {
-			FileInputStream pFile = new FileInputStream(CONFIG_FILE);
-			props.load(pFile);
-		}catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+    public PropertiesUtils() {
+    }
 
-	public Properties getRomaClientProperties() {
-		return props;
-	}
-
-	public void setTimeout(int timeout) {
-		props.setProperty("timeout", String.valueOf(timeout));
-	}
+    public static Properties getRomaClientProperties() {
+        Properties props = new Properties();
+        props.setProperty("address_port", DEFAULT_ROMA_ADDRESS);
+        props.setProperty("maxActive", DEFAULT_MAX_ACTIVE);
+        props.setProperty("maxIdle", DEFAULT_MAX_IDLE);
+        props.setProperty("timeout", DEFAULT_TIMEOUT);
+        props.setProperty("expTimeout", DEFAULT_EXP_TIMEOUT);
+        props.setProperty("bufferSize", DEFAULT_BUFFER_SIZE);
+        props.setProperty("maxRetry", DEFAULT_MAX_RETRY);
+        try {
+            FileInputStream pFile = new FileInputStream(CONFIG_FILE);
+            props.load(pFile);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return props;
+    }
 }
