cbb318848771875f191fae8611c7583b6f04bd68
firejun <fire@jun-t.com>
2013-01-07 18:10:48 +0900

    Change a little code for test.

--- romac4j/src/main/java/Start.java
+++ romac4j/src/main/java/Start.java
@@ -5,28 +5,34 @@ import com.rakuten.rit.roma.romac4j.RomaClient;
 import com.rakuten.rit.roma.romac4j.ValueReceiver;
 
 public class Start {
-	protected static Logger log = Logger.getLogger(Start.class.getName());
-	public static void main(String argv[]) throws Exception {
-		RomaClient rc = new RomaClient();
-		byte[] b = null;
-		long time0 = System.currentTimeMillis();
+    protected static Logger log = Logger.getLogger(Start.class.getName());
+
+    public static void main(String argv[]) throws Exception {
+        RomaClient rc = new RomaClient();
+        byte[] b = null;
+        rc.set("foo", "test1".getBytes(), 0);
+
+        boolean res = rc.cas("foo", 0, new Cas(null) {
+            public byte[] cas(ValueReceiver rcv) {
+                return "test2".getBytes();
+            }
+        });
+        log.debug("res: " + res);
+
+        long time0 = System.currentTimeMillis();
         for (int i = 0; i < 5000; i++) {
             try {
-                b = rc.get("foo");
-                log.debug(new String(b));
+            b = rc.get("foo");
+            rc.get("foo");
+            log.debug(new String(b));
             } catch (Exception e) {
-                log.error("Client Error.");
+                e.printStackTrace();
             }
             Thread.sleep(1000);
         }
-		boolean res = rc.cas("test", 0, new Cas(null){
-		    public byte[] cas(ValueReceiver rcv){
-		        return "aaa".getBytes();
-		    }
-		});
-		log.debug("res: " + res);
-		time0 = System.currentTimeMillis() - time0;
-		log.debug("Elap Time: " + time0/1000.0);
-		rc.destroy();
+        time0 = System.currentTimeMillis() - time0;
+        log.debug("Elap Time: " + time0 / 1000.0);
+
+        rc.destroy();
     }
 }
