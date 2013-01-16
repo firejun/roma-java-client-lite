9832b4da533aa26d4d733e537695453aa6a03d26
Junji Torii <jun930@gmail.com>
2013-01-16 16:39:22 +0900

    refactor : synchronization for a pool object

--- romac4j/src/main/java/com/rakuten/rit/roma/romac4j/pool/SocketPoolSingleton.java
+++ romac4j/src/main/java/com/rakuten/rit/roma/romac4j/pool/SocketPoolSingleton.java
@@ -1,8 +1,9 @@
 package com.rakuten.rit.roma.romac4j.pool;
 
-import java.io.IOException;
-import java.net.ConnectException;
+import java.net.SocketException;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.Map;
 
 import org.apache.commons.pool.PoolableObjectFactory;
 import org.apache.commons.pool.impl.GenericObjectPool;
@@ -14,6 +15,7 @@ public class SocketPoolSingleton {
     private static SocketPoolSingleton instance = new SocketPoolSingleton();
 
     private SocketPoolSingleton() {
+        poolMap = Collections.synchronizedMap(new HashMap<String, GenericObjectPool<Connection>>());
     }
 
     public static SocketPoolSingleton getInstance() {
@@ -21,65 +23,67 @@ public class SocketPoolSingleton {
     }
 
     private GenericObjectPool.Config config;
-    private HashMap<String, GenericObjectPool<Connection>> poolMap;
+    private Map<String, GenericObjectPool<Connection>> poolMap;
     private int timeout;
-    private int expTimeout;
     private int bufferSize;
 
-    public void setEnv(int maxActive, int maxIdle, int timeout, int expTimeout,
-            int bufferSize) {
-        poolMap = new HashMap<String, GenericObjectPool<Connection>>();
+    public void setEnv(int maxActive, int maxIdle, int timeout, int bufferSize) {
         config = new GenericObjectPool.Config();
         config.maxActive = maxActive;
         config.maxIdle = maxIdle;
         config.whenExhaustedAction = GenericObjectPool.WHEN_EXHAUSTED_GROW;
         //config.testOnBorrow = true;
         this.timeout = timeout;
-        this.expTimeout = expTimeout;
         this.bufferSize = bufferSize;
     }
 
     public synchronized Connection getConnection(String nodeId) throws Exception {
-        GenericObjectPool<Connection> pool = null;
-        Connection con = null;
-        String[] host = nodeId.split("_");
-        if (poolMap != null && poolMap.containsKey(nodeId)) {
-            pool = poolMap.get(nodeId);
-        } else {
-            PoolableObjectFactory<Connection> factory = new SocketPoolFactory(
-                    host[0], Integer.valueOf(host[1]));
+        GenericObjectPool<Connection> pool = poolMap.get(nodeId);
+        if (pool == null) {
+            PoolableObjectFactory<Connection> factory = 
+                    new SocketPoolFactory(nodeId, bufferSize);
             pool = new GenericObjectPool<Connection>(factory, config);
             poolMap.put(nodeId, pool);
         }
-        con = pool.borrowObject();
-        con.setNodeId(nodeId);
+        Connection con = pool.borrowObject();
         con.setSoTimeout(timeout);
-        con.setBufferSize(bufferSize);
 
         return con;
     }
 
-    public synchronized void deleteConnection(String nodeId) {
-        GenericObjectPool<Connection> pool = null;
-        if (poolMap.containsKey(nodeId)) {
-            pool = poolMap.get(nodeId);
-            pool.clear();
-        }
-        poolMap.remove(nodeId);
-    }
-
-    public synchronized void returnConnection(Connection con) {
-        GenericObjectPool<Connection> pool = null;
-        if (poolMap.containsKey(con.getNodeId())) {
-            pool = poolMap.get(con.getNodeId());
-            try {
-                con.setSoTimeout(expTimeout);
-                pool.returnObject(con);
-                poolMap.put(con.getNodeId(), pool);
-            } catch (Exception e) {
-                System.out.println("Can't return the Socket.");
-                e.printStackTrace();
+    public void deleteConnection(String nodeId) {
+        GenericObjectPool<Connection> pool = poolMap.get(nodeId);
+        if (pool != null) {
+            synchronized(pool) {
+                pool.clear();
+                poolMap.remove(nodeId);
             }
         }
+    }
+
+    public void returnConnection(Connection con) {
+        try {
+            con.setSoTimeout(0);
+        } catch (SocketException e) {
+            log.warn("returnConnection() : " + e.getMessage());
+            con.forceClose();
+            return;
+        }
+        
+        GenericObjectPool<Connection> pool = poolMap.get(con.getNodeId());
+        if (pool != null) {
+            synchronized(pool) {
+                if(poolMap.containsKey(con.getNodeId())) {
+                    try {
+                        pool.returnObject(con);
+                        return;
+                    } catch (Exception e) {
+                        log.error("returnConnection() : " + e.getMessage());
+                    }
+                }
+            }
+        }
+        log.info("returnConnection() : close connection");
+        con.forceClose();
     }
 }
