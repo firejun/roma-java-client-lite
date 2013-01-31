7b88f811cbe00dfe0559c6f89489542987c7fc2a
Junji Torii <jun930@gmail.com>
2013-01-31 22:37:33 +0900

    add gets command.

--- romac4j/src/main/java/com/rakuten/rit/roma/romac4j/RomaClient.java
+++ romac4j/src/main/java/com/rakuten/rit/roma/romac4j/RomaClient.java
@@ -2,6 +2,8 @@ package com.rakuten.rit.roma.romac4j;
 
 import java.io.IOException;
 import java.text.ParseException;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.apache.log4j.Logger;
 
@@ -26,8 +28,51 @@ public class RomaClient extends ClientObject {
         return ((ValueReceiver) rcv).getValueString();
     }
 
-    public byte[][] gets(String[] keys) throws IOException {
-        return null;
+    private ValueReceiver sendGetsCommand(String[] keys) throws IOException {
+        if(keys == null || keys.length == 0) return null;
+        if(keys.length == 1){
+            return (ValueReceiver)sendCmd(new ValueReceiver(), "get", keys[0], null, null);
+        }
+        String key = keys[0];
+        String opt = keys[1];
+        for(int i = 2; i < keys.length; i++){
+            if(keys[i].contains(" ")){
+                throw new IllegalArgumentException("Can't include space in a key.");
+            }
+            opt += " " + keys[i];
+        }
+        return (ValueReceiver)sendCmd(new ValueReceiver(), "gets", key, opt, null);
+    }
+    
+    
+    public Map<String, byte[]> gets(String[] keys) throws IOException {
+        ValueReceiver rcv = sendGetsCommand(keys);
+        if(rcv == null) return null;
+        
+        int len = rcv.size();
+        if(len == 0) return null;
+        
+        HashMap<String, byte[]> ret = new HashMap<String, byte[]>();
+        for(int i = 0; i < len; i++){
+            String key = rcv.getHeader(i).split(" ")[1];    // VALUE <key> <bytes> [<cas unique>]\r\n
+            ret.put(key, rcv.getValue(i));                  // <data block>\r\n
+        }
+        return ret;
+    }
+    
+    public Map<String, String> getsString(String[] keys) throws IOException {
+        ValueReceiver rcv = sendGetsCommand(keys);
+        if(rcv == null) return null;
+        
+        int len = rcv.size();
+        if(len == 0) return null;
+        
+        HashMap<String, String> ret = new HashMap<String, String>();
+        for(int i = 0; i < len; i++){
+            String key = rcv.getHeader(i).split(" ")[1];    // VALUE <key> <bytes> [<cas unique>]\r\n
+            ret.put(key, rcv.getValueString(i));            // <data block>\r\n
+        }
+        return ret;
     }
     
     private boolean set(String cmd, String key, byte[] value, int expt)
