ea9da99bd247430bc387ec03b0ab3315158b908a
Junji Torii <jun930@gmail.com>
2013-02-07 13:57:40 +0900

    add few methods.

--- romac4j/src/test/java/com/rakuten/rit/roma/romac4j/AListPluginTest.java
+++ romac4j/src/test/java/com/rakuten/rit/roma/romac4j/AListPluginTest.java
@@ -1,5 +1,7 @@
 package com.rakuten.rit.roma.romac4j;
 
+import java.util.Date;
+
 import junit.extensions.TestSetup;
 import junit.framework.Test;
 import junit.framework.TestCase;
@@ -59,6 +61,23 @@ public class AListPluginTest extends TestCase {
         }
     }
     
+    public void testGetsWithTime() throws Exception {
+        assertEquals(0, ac.getsWithTime("key1").length); // the key dose't exist
+        Date rightNow = new Date();
+        long epochtime = rightNow.getTime() / 1000;
+        
+        for(int i = 0;i < 10; i++){
+            assertTrue(ac.push("key1", "" + i)); // add data
+        }
+
+        AListPluginClient.VauleTime[] vt = ac.getsWithTime("key1");
+        assertEquals(10, vt.length);
+        for(int i = 0;i < 10; i++){
+            assertTrue(Math.abs(vt[i].getEpochTime() - epochtime) < 86400); // less than a day
+            assertEquals("" + i, vt[i].getValueString());
+        }
+    }
+    
     public void testAt01() throws Exception {
         for(int i = 0;i < 10; i++){
             assertNull(ac.atString("key1", i)); // the key dose't exist
@@ -86,8 +105,74 @@ public class AListPluginTest extends TestCase {
     }
     
     public void testClearEmpty02() throws Exception {
-        assertEquals(2, ac.isEmpty("key1")); // 2:the key dose't exist
+        assertEquals(2, ac.isEmpty("key1")); // 2: the key dose't exist
         assertFalse(ac.clear("key1")); // returns false when the key dose't exist
+    }
+    
+    public void testIsInclude01() throws Exception {
+        assertEquals(-2, ac.isInclude("key1", "0"));  // -2 : the key dose't exist
+        for(int i = 0;i < 10; i++){
+            assertTrue(ac.push("key1", "" + i)); // add data
+        }
+        for(int i = 0;i < 10; i++){
+            assertEquals(1, ac.isInclude("key1", "" + i));  // 1 : true
+            assertEquals(0, ac.isInclude("key1", "a" + i));  // 0 : false
+        }
+    }
+    
+    public void testIndex01() throws Exception {
+        assertEquals(-2, ac.index("key1", "0"));  // -2 : the key dose't exist
+        for(int i = 0;i < 10; i++){
+            assertTrue(ac.push("key1", "" + i)); // add data
+        }
+        for(int i = 0;i < 10; i++){
+            assertEquals(i, ac.index("key1", "" + i));
+        }
+        assertEquals(-3, ac.index("key1", "a0"));  // -3 : The value does not exist.
+    }
+    
+    public void testInsert01() throws Exception {
+        assertNull(ac.join("key1")); // the key dose't exist
+        assertTrue(ac.insert("key1", 0, "1"));
+        assertEquals("1", ac.join("key1"));
+        assertTrue(ac.insert("key1", 0, "0"));
+        assertEquals("0,1", ac.join("key1"));
+        assertTrue(ac.insert("key1", 5, "5"));        
+        assertEquals("0,1,,,,5", ac.join("key1"));
+        assertTrue(ac.insert("key1", 3, "3"));        
+        assertEquals("0,1,,3,,,5", ac.join("key1"));
+        
+        assertEquals("0", ac.atString("key1", 0));
+        assertEquals("1", ac.atString("key1", 1));
+        assertEquals("", ac.atString("key1", 2));
+        assertEquals("3", ac.atString("key1", 3));
+    }
+    
+    public void testSizedInsert01() throws Exception {
+        assertNull(ac.join("key1")); // the key dose't exist
+        String res = "";
+        for(int i = 0;i < 10; i++){
+            assertTrue(ac.sizedInsert("key1", 0, "" + i));  // unlimited
+            if(i == 0) res = "" + i;
+            else       res = i + "," + res;
+            assertEquals(res, ac.join("key1"));
+        }
+        for(int i = 0;i < 5; i++){
+            assertTrue(ac.sizedInsert("key2", 5, "" + i));  // list size = 5
+            if(i == 0) res = "" + i;
+            else       res = i + "," + res;
+            assertEquals(res, ac.join("key2"));
+        }
+        assertTrue(ac.sizedInsert("key2", 5, "5"));
+        assertEquals("5,4,3,2,1", ac.join("key2"));
+        assertTrue(ac.sizedInsert("key2", 5, "6"));
+        assertEquals("6,5,4,3,2", ac.join("key2"));
+        assertTrue(ac.sizedInsert("key2", 5, "7"));
+        assertEquals("7,6,5,4,3", ac.join("key2"));
+        assertTrue(ac.sizedInsert("key2", 5, "8"));
+        assertEquals("8,7,6,5,4", ac.join("key2"));
+        assertTrue(ac.sizedInsert("key2", 5, "9"));
+        assertEquals("9,8,7,6,5", ac.join("key2"));
     }
     
     public void testJoin01() throws Exception {
