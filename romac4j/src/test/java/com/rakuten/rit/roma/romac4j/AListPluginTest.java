340722481bf8c3451fa820b0ba08545a01a2fc5a
Junji Torii <jun930@gmail.com>
2013-02-13 15:38:11 +0900

    add methods

--- romac4j/src/test/java/com/rakuten/rit/roma/romac4j/AListPluginTest.java
+++ romac4j/src/test/java/com/rakuten/rit/roma/romac4j/AListPluginTest.java
@@ -175,6 +175,123 @@ public class AListPluginTest extends TestCase {
         assertEquals("9,8,7,6,5", ac.join("key2"));
     }
     
+    public void testSwapAndInsert01() throws Exception {
+        assertNull(ac.join("key1")); // the key dose't exist
+        for(int i = 0;i < 10; i++){
+            assertTrue(ac.swapAndInsert("key1", "" + i));
+        }
+        assertEquals("9,8,7,6,5,4,3,2,1,0", ac.join("key1"));
+        assertTrue(ac.swapAndInsert("key1", "0"));
+        assertEquals("0,9,8,7,6,5,4,3,2,1", ac.join("key1"));
+        assertTrue(ac.swapAndInsert("key1", "5"));
+        assertEquals("5,0,9,8,7,6,4,3,2,1", ac.join("key1"));
+        assertTrue(ac.swapAndInsert("key1", "5"));
+        assertEquals("5,0,9,8,7,6,4,3,2,1", ac.join("key1"));        
+    }
+    
+    public void testSwapAndSizedInsert01() throws Exception {
+        assertNull(ac.join("key1")); // the key dose't exist
+        String res = "";
+        for(int i = 0;i < 10; i++){
+            assertTrue(ac.swapAndSizedInsert("key1", 0, "" + i));  // unlimited
+            if(i == 0) res = "" + i;
+            else       res = i + "," + res;
+            assertEquals(res, ac.join("key1"));
+        }
+        assertTrue(ac.swapAndSizedInsert("key1", 5, "0"));  // unlimited
+        assertEquals("0,9,8,7,6", ac.join("key1"));
+        assertTrue(ac.swapAndSizedInsert("key1", 5, "7"));  // unlimited
+        assertEquals("7,0,9,8,6", ac.join("key1"));
+        assertTrue(ac.swapAndSizedInsert("key1", 5, "1"));  // unlimited
+        assertEquals("1,7,0,9,8", ac.join("key1"));
+        assertTrue(ac.swapAndSizedInsert("key1", 5, "1"));  // unlimited
+        assertEquals("1,7,0,9,8", ac.join("key1"));
+    }    
+    
+    public void testExpiredSwapAndInsert01() throws Exception {
+        assertNull(ac.join("key1")); // the key dose't exist
+        assertTrue(ac.push("key1", "0"));
+        assertTrue(ac.expiredSwapAndInsert("key1", 10, "1"));
+        assertEquals("1,0", ac.join("key1"));
+        assertTrue(ac.expiredSwapAndInsert("key1", 0, "2"));
+        assertEquals("2", ac.join("key1"));
+    }
+
+    public void testExpiredSwapAndInsert02() throws Exception {
+        assertNull(ac.join("key1")); // the key dose't exist
+        for(int i = 0;i < 10; i++){
+            assertTrue(ac.expiredSwapAndInsert("key1", 0, "" + i));
+            assertEquals("" + i, ac.join("key1"));
+        }
+    }
+
+    public void testExpiredSwapAndInsert03() throws Exception {
+        assertNull(ac.join("key1")); // the key dose't exist
+        for(int i = 0;i < 10; i++){
+            assertTrue(ac.expiredSwapAndInsert("key1", 10, "" + i));
+        }
+        assertEquals("9,8,7,6,5,4,3,2,1,0", ac.join("key1"));
+    }
+
+    public void testExpiredSwapAndInsert04() throws Exception {
+        assertNull(ac.join("key1")); // the key dose't exist
+        for(int i = 0;i < 10; i++){
+            assertTrue(ac.expiredSwapAndInsert("key1", 10, "" + i));
+        }
+        assertEquals("9,8,7,6,5,4,3,2,1,0", ac.join("key1"));
+        
+        assertTrue(ac.expiredSwapAndInsert("key1", 10, "0"));
+        assertEquals("0,9,8,7,6,5,4,3,2,1", ac.join("key1"));
+        assertTrue(ac.expiredSwapAndInsert("key1", 10, "7"));
+        assertEquals("7,0,9,8,6,5,4,3,2,1", ac.join("key1"));
+        assertTrue(ac.expiredSwapAndInsert("key1", 10, "1"));
+        assertEquals("1,7,0,9,8,6,5,4,3,2", ac.join("key1"));
+        assertTrue(ac.expiredSwapAndInsert("key1", 10, "1"));
+        assertEquals("1,7,0,9,8,6,5,4,3,2", ac.join("key1"));
+    }
+
+    public void testExpiredSwapAndSizedInsert01() throws Exception {
+        assertNull(ac.join("key1")); // the key dose't exist
+        assertTrue(ac.push("key1", "0"));
+        assertTrue(ac.expiredSwapAndSizedInsert("key1", 10, 5, "1"));
+        assertEquals("1,0", ac.join("key1"));
+        assertTrue(ac.expiredSwapAndSizedInsert("key1", 0, 5, "2"));
+        assertEquals("2", ac.join("key1"));
+    }
+
+    public void testExpiredSwapAndSizedInsert02() throws Exception {
+        assertNull(ac.join("key1")); // the key dose't exist
+        for(int i = 0;i < 10; i++){
+            assertTrue(ac.expiredSwapAndSizedInsert("key1", 0, 5, "" + i));
+            assertEquals("" + i, ac.join("key1"));
+        }
+    }
+
+    public void testExpiredSwapAndSizedInsert03() throws Exception {
+        assertNull(ac.join("key1")); // the key dose't exist
+        for(int i = 0;i < 10; i++){
+            assertTrue(ac.expiredSwapAndSizedInsert("key1", 10, 10, "" + i));
+        }
+        assertEquals("9,8,7,6,5,4,3,2,1,0", ac.join("key1"));
+    }
+    
+    public void testExpiredSwapAndSizedInsert04() throws Exception {
+        assertNull(ac.join("key1")); // the key dose't exist
+        for(int i = 0;i < 10; i++){
+            assertTrue(ac.expiredSwapAndSizedInsert("key1", 10, 10, "" + i));
+        }
+        assertEquals("9,8,7,6,5,4,3,2,1,0", ac.join("key1"));
+        
+        assertTrue(ac.expiredSwapAndSizedInsert("key1", 10, 5, "0"));
+        assertEquals("0,9,8,7,6", ac.join("key1"));
+        assertTrue(ac.expiredSwapAndSizedInsert("key1", 10, 5, "7"));
+        assertEquals("7,0,9,8,6", ac.join("key1"));
+        assertTrue(ac.expiredSwapAndSizedInsert("key1", 10, 5, "1"));
+        assertEquals("1,7,0,9,8", ac.join("key1"));
+        assertTrue(ac.expiredSwapAndSizedInsert("key1", 10, 5, "1"));
+        assertEquals("1,7,0,9,8", ac.join("key1"));
+    }
+    
     public void testJoin01() throws Exception {
         assertNull(ac.join("key1")); // the key dose't exist
         assertTrue(ac.push("key1", "0")); // add data
@@ -243,5 +360,195 @@ public class AListPluginTest extends TestCase {
         assertTrue(ac.push("key1", "2")); // add data
         assertEquals("0", ac.firstString("key1"));
         assertEquals("2", ac.lastString("key1"));
+    }
+    
+    public void testLength01() throws Exception {
+        assertEquals(-2, ac.length("key1")); // the key dose't exist
+        for(int i = 0;i < 10; i++){
+            assertTrue(ac.push("key1", "" + i)); // add data
+            assertEquals(i + 1, ac.length("key1"));
+        }
+        assertTrue(ac.clear("key1"));
+        assertEquals(0, ac.length("key1"));        
+    }
+    
+    public void testPop01() throws Exception {
+        assertNull(ac.popString("key1"));
+        for(int i = 0;i < 10; i++){
+            assertTrue(ac.push("key1", "" + i));
+        }
+        for(int i = 9;i >= 0; i--){
+            assertEquals("" + i, ac.popString("key1"));
+        }
+        assertNull(ac.popString("key1"));
+    }
+    
+    public void testSizedPush01() throws Exception {
+        assertNull(ac.join("key1")); // the key dose't exist
+        String res = "";
+        for(int i = 0;i < 10; i++){
+            assertTrue(ac.sizedPush("key1", 10, "" + i));
+            if(i == 0) res = "" + i;
+            else       res += "," + i;
+            assertEquals(res, ac.join("key1"));
+        }
+        for(int i = 0;i < 10; i++){
+            assertFalse(ac.sizedPush("key1", 10, "" + i));
+            assertEquals(res, ac.join("key1"));
+        }
+        assertTrue(ac.deleteAt("key1", 0));
+        assertEquals("1,2,3,4,5,6,7,8,9", ac.join("key1"));
+        assertTrue(ac.sizedPush("key1", 10, "10"));
+        assertEquals("1,2,3,4,5,6,7,8,9,10", ac.join("key1"));
+    }
+
+    public void testSwapAndPush01() throws Exception {
+        assertNull(ac.join("key1")); // the key dose't exist
+        for(int i = 0;i < 10; i++){
+            assertTrue(ac.swapAndPush("key1", "" + i));
+        }
+        assertEquals("0,1,2,3,4,5,6,7,8,9", ac.join("key1"));
+        assertTrue(ac.swapAndPush("key1", "0"));
+        assertEquals("1,2,3,4,5,6,7,8,9,0", ac.join("key1"));
+        assertTrue(ac.swapAndPush("key1", "5"));
+        assertEquals("1,2,3,4,6,7,8,9,0,5", ac.join("key1"));
+        assertTrue(ac.swapAndPush("key1", "5"));
+        assertEquals("1,2,3,4,6,7,8,9,0,5", ac.join("key1"));        
+    }
+    
+    public void testSwapAndSizedPush01() throws Exception {
+        assertNull(ac.join("key1")); // the key dose't exist
+        String res = "";
+        for(int i = 0;i < 10; i++){
+            assertTrue(ac.swapAndSizedPush("key1", 10, "" + i));
+            if(i == 0) res = "" + i;
+            else       res += "," + i;
+            assertEquals(res, ac.join("key1"));
+        }
+        for(int i = 0;i < 10; i++){
+            assertFalse(ac.swapAndSizedPush("key1", 10, "" + (i + 10)));
+            assertEquals(res, ac.join("key1"));
+        }
+        assertTrue(ac.deleteAt("key1", 0));
+        assertEquals("1,2,3,4,5,6,7,8,9", ac.join("key1"));
+        assertTrue(ac.swapAndSizedPush("key1", 10, "10"));
+        assertEquals("1,2,3,4,5,6,7,8,9,10", ac.join("key1"));
+
+        assertTrue(ac.swapAndSizedPush("key1", 10, "5"));
+        assertEquals("1,2,3,4,6,7,8,9,10,5", ac.join("key1"));
+        assertTrue(ac.swapAndSizedPush("key1", 10, "6"));
+        assertEquals("1,2,3,4,7,8,9,10,5,6", ac.join("key1"));
+        assertTrue(ac.swapAndSizedPush("key1", 10, "6"));
+        assertEquals("1,2,3,4,7,8,9,10,5,6", ac.join("key1"));
+    }
+    
+    public void testExpiredSwapAndPush01() throws Exception {
+        assertNull(ac.join("key1")); // the key dose't exist
+        assertTrue(ac.push("key1", "0"));
+        assertTrue(ac.expiredSwapAndPush("key1", 10, "1"));
+        assertEquals("0,1", ac.join("key1"));
+        assertTrue(ac.expiredSwapAndPush("key1", 0, "2"));
+        assertEquals("2", ac.join("key1"));
+    }
+    
+    public void testExpiredSwapAndPush02() throws Exception {
+        assertNull(ac.join("key1")); // the key dose't exist
+        for(int i = 0;i < 10; i++){
+            assertTrue(ac.expiredSwapAndPush("key1", 0, "" + i));
+            assertEquals("" + i, ac.join("key1"));
+        }
+    }
+
+    public void testExpiredSwapAndPush03() throws Exception {
+        assertNull(ac.join("key1")); // the key dose't exist
+        for(int i = 0;i < 10; i++){
+            assertTrue(ac.expiredSwapAndPush("key1", 10, "" + i));
+        }
+        assertEquals("0,1,2,3,4,5,6,7,8,9", ac.join("key1"));
+    }
+
+    public void testExpiredSwapAndPush04() throws Exception {
+        assertNull(ac.join("key1")); // the key dose't exist
+        for(int i = 0;i < 10; i++){
+            assertTrue(ac.expiredSwapAndPush("key1", 10, "" + i));
+        }
+        assertEquals("0,1,2,3,4,5,6,7,8,9", ac.join("key1"));
+        
+        assertTrue(ac.expiredSwapAndPush("key1", 10, "0"));
+        assertEquals("1,2,3,4,5,6,7,8,9,0", ac.join("key1"));
+        assertTrue(ac.expiredSwapAndPush("key1", 10, "7"));
+        assertEquals("1,2,3,4,5,6,8,9,0,7", ac.join("key1"));
+        assertTrue(ac.expiredSwapAndPush("key1", 10, "1"));
+        assertEquals("2,3,4,5,6,8,9,0,7,1", ac.join("key1"));
+        assertTrue(ac.expiredSwapAndPush("key1", 10, "1"));
+        assertEquals("2,3,4,5,6,8,9,0,7,1", ac.join("key1"));
+    }
+    
+    public void testExpiredSwapAndSizedPush01() throws Exception {
+        assertNull(ac.join("key1")); // the key dose't exist
+        assertTrue(ac.push("key1", "0"));
+        assertTrue(ac.expiredSwapAndSizedPush("key1", 10, 5, "1"));
+        assertEquals("0,1", ac.join("key1"));
+        assertTrue(ac.expiredSwapAndSizedPush("key1", 0, 5, "2"));
+        assertEquals("2", ac.join("key1"));
+    }
+    
+    public void testExpiredSwapAndSizedPush02() throws Exception {
+        assertNull(ac.join("key1")); // the key dose't exist
+        for(int i = 0;i < 10; i++){
+            assertTrue(ac.expiredSwapAndSizedPush("key1", 0, 5, "" + i));
+            assertEquals("" + i, ac.join("key1"));
+        }
+    }
+
+    public void testExpiredSwapAndSizedPush03() throws Exception {
+        assertNull(ac.join("key1")); // the key dose't exist
+        for(int i = 0;i < 5; i++){
+            assertTrue(ac.expiredSwapAndSizedPush("key1", 10, 5, "" + i));
+        }
+        for(int i = 0;i < 5; i++){
+            assertFalse(ac.expiredSwapAndSizedPush("key1", 10, 5, "" + (i + 5)));
+        }
+        assertEquals("0,1,2,3,4", ac.join("key1"));
+    }
+
+    public void testExpiredSwapAndSizePush04() throws Exception {
+        assertNull(ac.join("key1")); // the key dose't exist
+        for(int i = 0;i < 5; i++){
+            assertTrue(ac.expiredSwapAndSizedPush("key1", 10, 5, "" + i));
+        }
+        assertEquals("0,1,2,3,4", ac.join("key1"));
+
+        assertTrue(ac.expiredSwapAndSizedPush("key1", 10, 5, "0"));
+        assertEquals("1,2,3,4,0", ac.join("key1"));
+        assertTrue(ac.expiredSwapAndSizedPush("key1", 10, 5, "2"));
+        assertEquals("1,3,4,0,2", ac.join("key1"));
+        assertTrue(ac.expiredSwapAndSizedPush("key1", 10, 5, "1"));
+        assertEquals("3,4,0,2,1", ac.join("key1"));
+        assertTrue(ac.expiredSwapAndSizedPush("key1", 10, 5, "1"));
+        assertEquals("3,4,0,2,1", ac.join("key1"));
+    }
+
+    public void testUpdateAt01() throws Exception {
+        assertEquals(-2, ac.updateAt("key1", 0, "0"));  // NOT_FOUND
+        for(int i = 0;i < 10; i++){
+            assertTrue(ac.push("key1", "" + i));
+        }
+        assertEquals("0,1,2,3,4,5,6,7,8,9", ac.join("key1"));
+        assertEquals(1, ac.updateAt("key1", 0, "10")); // STORED
+        assertEquals("10,1,2,3,4,5,6,7,8,9", ac.join("key1"));
+        assertEquals(-2, ac.updateAt("key1", 20, "0"));  // NOT_FOUND
+        assertEquals(-2, ac.updateAt("key1", -1, "20")); // NOT_FOUND
+    }
+    
+    public void testShift01() throws Exception {
+        assertNull(ac.popString("key1"));
+        for(int i = 0;i < 10; i++){
+            assertTrue(ac.push("key1", "" + i));
+        }
+        for(int i = 0;i < 10; i++){
+            assertEquals("" + i, ac.shiftString("key1"));
+        }
+        assertNull(ac.shiftString("key1"));
     }
 }
